414e8136ba9d0b35e286bcdf4c771e04fc1609c5
Khalil Benzahra <khalil.benzahra@itbs.tn>
2026-05-10 02:31:54 +0100

    Ajouter le module historique frontend/backend integration

--- src/main/java/com/gestion/gestionretours/repository/RetourProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gestion.gestionretours.repository;
-
-import com.gestion.gestionretours.entity.RetourProduit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RetourProduitRepository extends JpaRepository<RetourProduit, Long> {
-}
